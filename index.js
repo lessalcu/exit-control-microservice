85420b19ed8db2efbfc1e89410a844e8f9d1f9ba
Salomé Salas <154089468+lessalcu@users.noreply.github.com>
2025-02-12 21:25:03 -0500

    feat: :sparkles:

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');  // Import CORS
 const exitRoutes = require('./src/routes/exitRoutes');
+
 const db = require('./src/config/db');
 
 const app = express();
+
 const PORT = process.env.PORT || 7001;
+app.use(cors()); 
 
 app.use(express.json());
 app.use('/exit', exitRoutes);
